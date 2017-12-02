79a09f7c1d0ce5543fa9765a36fae4cfa05de409
daniel <daniel_amar@berkeley.edu>
2017-12-01 17:51:31 -0800

    update

--- picoproapp/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidthings.myproject;
 
+import android.app.AlarmManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -343,6 +344,13 @@ public class MainActivity extends AppCompatActivity {
     public void updateLogoWeather(int newLogo){
         ((MyVoilaApp) this.getApplication()).setLogoWeather(newLogo);
     }
+    public void updateTimeClock(int newTime){
+        //((MyVoilaApp) this.getApplication()).setLogoWeather(newTime);
+        Calendar c = Calendar.getInstance();
+        c.set(2017, 12, 1, 17, 36, 27);
+        AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        am.setTime(c.getTimeInMillis());
+    }
 
     /**
      * Callback to handle incoming requests to the GATT server.
@@ -379,6 +387,10 @@ public class MainActivity extends AppCompatActivity {
                     int logoWeatherUpdated = bitArrayToInt(value);
                     System.out.println("new Logo Weather BLE: "+logoWeatherUpdated);
                     updateLogoWeather(logoWeatherUpdated);
+                }else if (value[0]==4){ //time clock info received
+                    int timeUpdated = bitArrayToInt(value);
+                    System.out.println("new Time BLE: "+timeUpdated);
+                    updateTimeClock(timeUpdated);
                 }
                 reloadMainActivity();
 
